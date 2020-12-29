45c46d8c10b7a7212755581b3e7beb9cdedeccf1
Luan Nico <luanpotter27@gmail.com>
2020-12-28 19:18:53 -0500

    Add linter

---
+++ scripts/lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ../game
+
+FORMAT_ISSUES=$(flutter format --set-exit-if-changed -n .)
+if [ $? -eq 1 ]; then
+  echo "flutter format issues on"
+  echo $FORMAT_ISSUES
+  exit 1
+fi
+
+flutter pub get
+
+ANALYZE_ISSUES=$(flutter analyze .)
+if ! echo "$result" | grep -q "No issues found!"; then
+  echo "flutter analyze issue: $1"
+  echo "$ANALYZE_ISSUES"
+  exit 1
+fi
+
+echo "success"
+exit 0
